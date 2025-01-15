4fc0c004d8eb36fa71acf6296f3b992cb4b09c3a
FerdiaMT <oscadot@gmail.com>
2025-01-15 16:11:20 +0000

    implemented ambient lighting

--- OpenGlCourseApp/OpenGlCourseApp/Main.cpp
+++ OpenGlCourseApp/OpenGlCourseApp/Main.cpp
@@ -17,6 +17,7 @@
 #include "Shader.h"
 #include "Camera.h"
 #include "Texture.h"
+#include "Light.h"
 
 const float toRadians = 3.14159265f / 180.0f;
 
@@ -27,6 +28,8 @@ std::vector<Shader> shaderList;
 
 Texture brickTexture; //these would normally be in the mesh class 
 Texture dirtTexture;
+
+Light mainLight;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastTime = 0.0f;
@@ -58,8 +61,6 @@ void CreateObjects()
 		0.0f, -1.0f, 1.0f   ,0.5f,0.0f,
 		1.0f, -1.0f, 0.0f   ,1.0f,0.0f,
 		0.0f, 1.0f, 0.0f    ,0.5f,1.0f,
-
-
 	};
 
 
@@ -96,9 +97,9 @@ int main()
 	dirtTexture = Texture("Textures/dirt.png");
 	dirtTexture.LoadTexture();
 
-	
+	mainLight = Light(1.0f,1.0f,1.0f,1.0f);
 
-	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformAmbientIntensity=0, uniformAmbientColour=0;
 	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
 
 	// Loop until window closed
@@ -124,7 +125,10 @@ int main()
 		uniformModel = shaderList[0].GetModelLocation();
 		uniformProjection = shaderList[0].GetProjectionLocation();
 		uniformView = shaderList[0].GetViewLocation();
+		uniformAmbientColour = shaderList[0].GetAmbientColourLocation();
+		uniformAmbientIntensity = shaderList[0].GetAmbientIntensityLocation();
 
+		mainLight.UseLight(uniformAmbientIntensity, uniformAmbientColour);
 
 		glm::mat4 model(1.0f);	
 
